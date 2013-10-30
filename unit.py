58a398d56b328341e966a6993e4470a95db4cba0
Mike Wiederhold <mike@couchbase.com>
2013-10-30 16:37:51 -0700

    Add better upr open tests

--- unit.py
+++ unit.py
@@ -11,40 +11,70 @@ PORT = 12000
 
 class UprTestCase(unittest.TestCase):
     def setUp(self):
-        self.client = UprClient(HOST, PORT)
+        self.upr_client = UprClient(HOST, PORT)
+        self.mcd_client = McdClient(HOST, PORT)
 
     def tearDown(self):
-        self.client.shutdown() 
+        self.upr_client.shutdown()
+        self.mcd_client.shutdown()
 
+    """Basic upr open consumer connection test
+
+    Verifies that when the open upr consumer command is used there is a
+    connection instance that is created on the server and that when the
+    tcp connection is closed the connection is remove from the server"""
     def test_open_consumer_connection_command(self):
-        op = self.client.open_consumer("mystream")
+        op = self.upr_client.open_consumer("mystream")
         response = op.next_response()
         assert response['status'] == SUCCESS
 
+        op = self.mcd_client.stats('tap')
+        response = op.next_response()
+        assert response['value']['mystream:type'] == 'consumer'
+
+        self.upr_client.shutdown()
+        op = self.mcd_client.stats('tap')
+        response = op.next_response()
+        assert 'mystream:type' not in response['value']
+
+    """Basic upr open producer connection test
+
+    Verifies that when the open upr producer command is used there is a
+    connection instance that is created on the server and that when the
+    tcp connection is closed the connection is remove from the server"""
     def test_open_producer_connection_command(self):
-        op = self.client.open_producer("mystream")
+        op = self.upr_client.open_producer("mystream")
         response = op.next_response()
         assert response['status'] == SUCCESS
+
+        op = self.mcd_client.stats('tap')
+        response = op.next_response()
+        assert response['value']['mystream:type'] == 'producer'
+
+        self.upr_client.shutdown()
+        op = self.mcd_client.stats('tap')
+        response = op.next_response()
+        assert 'mystream:type' not in response['value']
 
     @unittest.skip("Add stream response is broken in memcached")
     def test_add_stream_command(self):
-        op = self.client.add_stream(0, 0)
+        op = self.upr_client.add_stream(0, 0)
         response = op.next_response()
         assert response['status'] == ERR_NOT_SUPPORTED
 
     def test_close_stream_command(self):
-        op = self.client.close_stream(0)
+        op = self.upr_client.close_stream(0)
         response = op.next_response()
         assert response['status'] == ERR_NOT_SUPPORTED
 
     def test_get_failover_log_command(self):
-        op = self.client.get_failover_log(0)
+        op = self.upr_client.get_failover_log(0)
         response = op.next_response()
         assert response['status'] == ERR_NOT_SUPPORTED
 
     @unittest.skip("Causes issue with test rerun")
     def test_stream_request_command(self):
-        op = self.client.stream_req(0, 0, 0, 0, 0, 0)
+        op = self.upr_client.stream_req(0, 0, 0, 0, 0, 0)
         response = op.next_response()
         assert response['status'] == ERR_NOT_SUPPORTED
 
