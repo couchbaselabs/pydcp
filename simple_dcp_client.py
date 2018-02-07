c29e34060fa032914eae14cd47d78b5ab2673b91
Jim Walker <jim@couchbase.com>
2018-02-07 10:52:51 +0000

    Update with DCP delete time support
    Allow the client to open DCP with delete times and
    correctly receive the new packet format.

--- simple_dcp_client.py
+++ simple_dcp_client.py
@@ -40,9 +40,9 @@ def handleSystemEvent(response):
     if response['event'] == EVENT_CREATE_COLLECTION:
         print "DCP Event: Collection {} created at seqno: {}".format(response['key'],response['seqno'])
     elif response['event'] == EVENT_DELETE_COLLECTION:
-        print "DCP Event: Collection {} deleted at seqno: {}".format(response['key'],response['seqno'])    
+        print "DCP Event: Collection {} deleted at seqno: {}".format(response['key'],response['seqno'])
     elif response['event'] == EVENT_COLLECTION_SEPARATOR:
-        print "DCP Event: Collection Separator changed to {} at seqno: {}".format(response['key'],response['seqno'])    
+        print "DCP Event: Collection Separator changed to {} at seqno: {}".format(response['key'],response['seqno'])
     else:
         print "Unknown DCP Event:",response['event']
 
@@ -58,7 +58,7 @@ def handleMutation(response):
         else:
             output_string = "KEY:" + response['key']
     if args.docs:
-        output_string += "BODY:" + response['value']   
+        output_string += "BODY:" + response['value']
     if args.xattrs:
         if 'xattrs' in response:
             output_string += " XATTRS:" + response['xattrs']
@@ -85,13 +85,17 @@ def process_dcp_traffic(stream,args):
             elif response['opcode'] == CMD_MUTATION:
                 handleMutation(response)
                 key_count += 1
+            elif response['opcode'] == CMD_DELETION:
+                print response
+                #handleMutation(response)
+                key_count += 1
             elif response['opcode'] == CMD_SNAPSHOT_MARKER:
                 print "\nReceived snapshot marker"
             elif response['opcode'] == CMD_SYSTEM_EVENT:
                 handleSystemEvent(response)
             else:
                 print 'Unhandled opcode:',response['opcode']
-        else: 
+        else:
             print '\nNo response'
     print "closing stream"
     dcp_client.close()
@@ -103,6 +107,7 @@ def add_stream(args):
     start_seq_no = args.start
     end_seq_no = args.end
     stream_xattrs = args.xattrs
+    include_delete_times = args.delete_times
     stream_collections = args.collections
     filter_file = args.filter
     filter_json = ''
@@ -126,8 +131,11 @@ def add_stream(args):
         filter_json = filter_file.read()
         print "DCP Open filter: {}".format(filter_json)
 
-    response = dcp_client.open_producer("python stream",xattr=stream_xattrs, 
-                                        collections=stream_collections, json=filter_json)
+    response = dcp_client.open_producer("python stream",
+                                        xattr=stream_xattrs,
+                                        delete_times=include_delete_times,
+                                        collections=stream_collections,
+                                        json=filter_json)
     print response
     assert response['status'] == SUCCESS
     print "Opened DCP consumer connection"
@@ -138,7 +146,7 @@ def add_stream(args):
              start_seqno=start_seq_no, end_seqno=end_seq_no, vb_uuid=0)
     handle_stream_create_response(stream)
     return stream
-       
+
 def parseArguments():
   parser = argparse.ArgumentParser(description='Create a simple DCP Consumer')
   parser.add_argument('--node', '-n', default="localhost:11210", help='Cluster Node to connect to (host:port)')
@@ -151,6 +159,7 @@ def parseArguments():
   parser.add_argument('--keys', '-k', help='Dump keys', default=False, action="store_true")
   parser.add_argument('--docs', '-d', help='Dump document', default=False, action="store_true")
   parser.add_argument("--filter", '-f', help="DCP Filter", required=False)
+  parser.add_argument("--delete_times", help="Include delete times", default=False, required=False, action="store_true")
   parser.add_argument("-u", "--user", help="User", required=True)
   parser.add_argument("-p", "--password", help="Password", required=True)
   return parser.parse_args()
